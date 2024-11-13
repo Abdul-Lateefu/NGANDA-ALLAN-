c65563c9c7a69a13804dae36080947d2e6a3ffb5
Abdul-Lateefu <kikoabdiziyech@gmail.com>
2024-11-14 00:30:54 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+﻿let currentIndex = 0;
+const images = document.querySelectorAll('.carousel-image');
+const prev = document.querySelector('.prev');
+const next = document.querySelector('.next');
+
+function showImage(index) {
+    images.forEach((img, i) => img.classList.toggle('active', i === index));
+}
+
+function showNextImage() {
+    currentIndex = (currentIndex + 1) % images.length;
+    showImage(currentIndex);
+}
+
+function showPrevImage() {
+    currentIndex = (currentIndex - 1 + images.length) % images.length;
+    showImage(currentIndex);
+}
+
+next.addEventListener('click', showNextImage);
+prev.addEventListener('click', showPrevImage);
+
+setInterval(showNextImage, 3000); 
